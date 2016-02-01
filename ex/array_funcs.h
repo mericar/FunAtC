45ae05b2f0ad68e2e9de60f54a40377ed0fa027a
Mirko Carich <mirkocarich@gmail.com>
2016-01-31 18:43:36 -0800

    Update array_funcs.h

--- ex/array_funcs.h
+++ ex/array_funcs.h
@@ -1,1 +1,42 @@
 
+//
+//  array_funcs.h
+//  cplay
+//
+//  Created by M on 2016-01-31.
+//  Copyright © 2016 mec. All rights reserved.
+//
+
+#ifndef array_funcs_h
+#define array_funcs_h
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+//Array Struct
+typedef struct arr {
+    int *array;
+    int size;
+} SizedArray;
+
+// Makes an array of some size
+SizedArray * make_sized_array(int size);
+
+// Pushes an int to a given index of an int array
+void push_int(int a, int *b);
+
+// Returns true if given int is contained within the given array
+bool does_contain(int a, int *b);
+
+//  Takes an int as argument, returns a pointer to a new array
+//      containing a dictionary pointing to all values and their
+//      index in their extracted array.
+int* pull_all_int(int a, int *b);
+
+// Takes some index of an array and replaces the existing value with zero
+int* zero_index(int* a);
+
+
+#endif /* array_funcs_h */
