9b721ed09058f74a4ec9306a63b070d979832362
Mirko <mirkocarich@gmail.com>
2016-05-01 22:02:30 -0700

    Added plain ol' English for review

--- algs/selection_sort.c
+++ algs/selection_sort.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 
+// Does a swap ... takes an int pointer i, and j.
+// creates a temporary variable t, stores the value pointed to by i.
+// the value pointed to by i is assigned the value pointed to by j
+// the value pointed to by j is assigned t, the temp variable storing
+//	the now gone prior value pointed to by i
 void swap(int* i, int* j){
    	int t = *i;
    	*i = *j;
    	*j = t;
    }
-
-void selection_sort(int a[], int l, int r){
+   
+// Time Complexity for avg, best, and worst cases: O(n^2)
+//Params: takes an int array, and array length.
+// LOOP:
+// Loops over array length - 1 times.
+// In each iteration, min is assigned i.
+// another loops iterates through the remaining indices,
+//	storing the index representing the minimum value (some j index)
+// After the minimum value index has been discovered, and the minimum index isn't equal to the j index,
+//	we swap the values using our swap function above.
+// and GOTO LOOP above ^
+void selection_sort(int a[], int length){
 	int i,j;
-	for (int i = l; i < r; i++){
-		int min = i;
-		for (j = i+1; j <= r; j++){
+	for (int i = 0; i < length-1; i++){
+		int min_index = i;
+		for (j = i+1; j <= length; j++){
 			if (a[j] < a[min]) {
-				min = j;
+				min_index = j;
 			}
 		}
-		swap(&a[i], &a[min]);
+		if(min_index != j){
+			swap(&a[i], &a[min]);
+		}
 	}
 }
 
@@ -31,7 +48,7 @@ int main() {
 	}
 
 	printf("%s\n", "break before sort");
-	selection_sort(nums, 0, 9);
+	selection_sort(nums, 9);
 	printf("%s\n", "break after sort");
 
 	for (int i = 0; i < len; ++i)
