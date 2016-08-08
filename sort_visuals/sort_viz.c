7d4905db769687f8d7523695b8f37db4e1bdd224
mirkmasterC <mirkocarich@gmail.com>
2016-08-07 23:23:04 -0700

    functioning sort visualising mechanism added.

--- sort_visuals/sort_viz.c
+++ sort_visuals/sort_viz.c
@@ -11,6 +11,46 @@ const char UNIT = 'o';
 const char BLANK = ' ';
 
 
+/*
+ * Selection Sort is added here for demonstrating the use cases of the visualiser
+ *
+ */
+
+// Does a swap ... takes an int pointer i, and j.
+// creates a temporary variable t, stores the value pointed to by i.
+// the value pointed to by i is assigned the value pointed to by j
+// the value pointed to by j is assigned t, the temp variable storing
+//	the now gone prior value pointed to by i
+void swap(int* i, int* j){
+   	int t = *i;
+   	*i = *j;
+   	*j = t;
+}
+
+// Time Complexity for avg, best, and worst cases: O(n^2)
+//Params: takes an int array, and array length.
+// LOOP:
+// Loops over array length - 1 times.
+// In each iteration, min is assigned i.
+// another loops iterates through the remaining indices,
+//	storing the index representing the minimum value (some j index)
+// After the minimum value index has been discovered, and the minimum index isn't equal to the j index,
+//	we swap the values using our swap function above.
+// and GOTO LOOP above ^
+void selection_sort(int a[], int length){
+    int i,j;
+    for (i = 0; i < length-1; i++){
+        int min_index = i;
+        for (j = i+1; j <= length; j++){
+            if (a[j] < a[min_index]) {
+                min_index = j;
+            }
+        }
+        if(min_index != j){
+            swap(&a[i], &a[min_index]);
+        }
+    }
+}
 
 
 struct ray {
@@ -77,6 +117,8 @@ int main(){
     
     
     //test
+    ray_of_ints_to_matrix_picture(&list_of_int);
+    selection_sort(list_of_int.array, list_of_int.length);
     ray_of_ints_to_matrix_picture(&list_of_int);
     
     
